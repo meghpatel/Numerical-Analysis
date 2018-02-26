17aa1dd0135b9e8d4ca54e034f2b8669332453a6
meghpatel <patel.megh09@gmail.com>
2018-02-27 03:47:28 +0530

    Started to add logic. May or maynot be working.

--- GuassElimination.cpp
+++ GuassElimination.cpp
@@ -4,17 +4,18 @@
 #include<iostream>
 using namespace std;
 
-void display(int **,int,int);//prints array
+void display(float **,int,int);//prints array
+float** guassEle(float **,int,int);
 
 int main()
 {
 	cout<<"Enter the dimentions of matrix-RowXColumns"<<endl;
 	int m,n;
 	cin>>m>>n;
-	int **a = (int **)malloc(m * sizeof(int *));
+	float **a = (float **)malloc(m * sizeof(float *));
 	for(int i=0;i<m;i++)
 	{
-		a[i] = (int *)malloc(n * sizeof(int));
+		a[i] = (float *)malloc(n * sizeof(float));
 	}
 	cout<<"\nEnter the Array"<<endl;
 	for(int i=0;i<m;i++)
@@ -29,7 +30,7 @@ int main()
 	return 0;
 }
 
-void display(int **a,int m,int n)
+void display(float **a,int m,int n)
 {
 	for(int i=0;i<m;i++)
 	{
@@ -38,5 +39,21 @@ void display(int **a,int m,int n)
 			cout<<" "<<*(*(a+i)+j);
 		}
 		cout<<endl;
+	}
+}
+
+float** guassEle(float** a,int m,int n)
+{
+	int temp;
+	for(int i=0;i<m-1,i++)
+	{
+		for(int k=0;k<n-i-1;k++)
+		{
+			temp=-(a[k+1][j]/a[k][j]);
+			for(int j=0;j<n;j++)
+			{
+				a[k+1][j] = temp*a[k][j] + a[k+1][j];
+			}
+		}
 	}
 }
