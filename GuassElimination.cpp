08a2c2e5e6575461a0a6dcfcdd263b10e493d83a
meghpatel <patel.megh09@gmail.com>
2018-02-27 02:58:35 +0530

    Basic IO Works

--- GuassElimination.cpp
+++ GuassElimination.cpp
@@ -3,8 +3,32 @@
 #include<iostream>
 using namespace std;
 
+void display();//prints array
+
 int main()
 {
-
+	cout<<"Enter the dimentions of matrix-RowXColumns"<<endl;
+	int m,n;
+	cin>>m>>n;
+	int a[m][n];
+	cout<<"\nEnter the Array"<<endl;
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cout<<"Enter a["<<i<<"]["<<j<<"]: ";
+			cin>>a[i][j]; 
+		}
+	}
+	
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cout<<" "<<a[i][j];
+		}
+		cout<<endl;
+	}
+	
 	return 0;
 }
