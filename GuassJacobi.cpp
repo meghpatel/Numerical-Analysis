596bb7e7688b0a5e4df731565f6af7a770bd6b41
meghpatel <patel.megh09@gmail.com>
2018-03-13 12:26:31 +0530

    First Iteration works

--- GuassJacobi.cpp
+++ GuassJacobi.cpp
@@ -7,23 +7,49 @@ int main()
 	cout<<"Enter the number of equations: "<<endl;
 	int n;
 	cin>>n;
-	float a[n][n], const[n];
+	float coeff[n][n], cons[n];
 	cout<<"Enter the coefficients of Equations."<<endl;
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<n;j++)
 		{
 			cout<<"Enter the value of equation "<<i<<" and coefficient "<<j<<" :"<<endl;
-			cin>>a[i][j];
+			cin>>coeff[i][j];
 		}
+		cout<<"Enter the constant term: "<<endl;
+		cin>>cons[i];
 	}
-	int x[n]={0};
+	float x[n];
 	for(int i=0;i<n;i++)
 	{
-		for(int j=0;j<n;j++)
+		x[i] = 0.0;
+	}
+	int t=0,m;
+	float sum=0;
+	cout<<"Enter the number of iterations: "<<endl;
+	cin>>m;
+	for(int k=0;k<1;k++)
+	{
+		for(int i=0;i<n;i++)
 		{
-			x[j] = ()/a[i][]
+			sum = cons[i];
+			for(int j=0;j<n;j++)
+			{
+				if(i==j)
+				{
+					continue;
+				}
+				sum = sum - coeff[i][j]*x[j];
+			}
+			sum = sum/coeff[i][t];
+			t++;
+			x[i] = sum;
 		}
+		for(int i=0;i<n;i++)
+		{
+			printf("%f ",x[i]);
+		}
+		printf("\n");
 	}
 }
 
