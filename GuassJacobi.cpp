aa937bc7535c86e87d2d9c2eb344d55de0b50620
meghpatel <patel.megh09@gmail.com>
2018-03-13 11:41:07 +0530

    Basic Input"

--- GuassJacobi.cpp
+++ GuassJacobi.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main()
 {
-	
+	cout<<"Enter the number of equations: "<<endl;
+	int n;
+	cin>>n;
+	float a[n][n], const[n];
+	cout<<"Enter the coefficients of Equations."<<endl;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			cout<<"Enter the value of equation "<<i<<" and coefficient "<<j<<" :"<<endl;
+			cin>>a[i][j];
+		}
+	}
+	int x[n]={0};
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			x[j] = ()/a[i][]
+		}
+	}
 }
+
+
